5877fec6b9a7f18e1e4ce9fd28a8272930480ad5
Guillaume Sere <guillaumesere@hotmail.fr>
2023-02-18 11:09:32 +0100

    Updates

---
+++ precache-manifest.2ef17dc6cdda424f14ea6fe81c38529c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5121f2aee3545a4be16c609fdefcd5",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "53b7c06e0aa21e7c65bd",
+    "url": "/react-tetris/static/css/main.6b7ee09b.chunk.css"
+  },
+  {
+    "revision": "205ed7c73228de915bd6",
+    "url": "/react-tetris/static/js/2.c4b5ed7a.chunk.js"
+  },
+  {
+    "revision": "53b7c06e0aa21e7c65bd",
+    "url": "/react-tetris/static/js/main.d2f3f477.chunk.js"
+  },
+  {
+    "revision": "9f8e8f985e23fe474e69",
+    "url": "/react-tetris/static/js/runtime~main.9a7ed037.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "f87481078f258ccbc7070168db49a661",
+    "url": "/react-tetris/static/media/bg.f8748107.png"
+  }
+]);
